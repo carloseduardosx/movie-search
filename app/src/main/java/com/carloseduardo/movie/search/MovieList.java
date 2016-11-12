23337686872ac95d9e95b70877882866d1b3e783
Carlos Eduardo <carloseduardosx2015@gmail.com>
2016-11-12 00:47:19 -0300

    Create project

---
+++ app/src/main/java/com/carloseduardo/movie/search/MovieList.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.movie.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_list);
+    }
+}
